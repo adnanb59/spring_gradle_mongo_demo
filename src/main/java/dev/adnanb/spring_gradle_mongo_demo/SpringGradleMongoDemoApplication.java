eea97b3a6b1e6b2a6e9da946eabe9fd6887e5b69
Adnan Bhuiyan <adnanb59@users.noreply.github.com>
2020-06-19 17:05:37 -0400

    Add files via upload

---
+++ src/main/java/dev/adnanb/spring_gradle_mongo_demo/SpringGradleMongoDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.adnanb.spring_gradle_mongo_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGradleMongoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGradleMongoDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/adnanb/spring_gradle_mongo_demo/SpringGradleMongoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.adnanb.spring_gradle_mongo_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGradleMongoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
